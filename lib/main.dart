c90bcd3baa6e33dfc496817c69ad7bb528bc9f85
NrupParikh <108717119+NrupParikh@users.noreply.github.com>
2023-06-13 14:57:45 +0530

    - Show loader until response comes. - Log added

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    int days = 7;
+    int days = 30;
     String name = "Nrup";
 
     return MaterialApp(
